cd5da1dc7c8a46105e016b0ca274d1934492974c
Hades.cool <723760950@qq.com>
2017-07-19 16:20:40 +0800

    IModelCallBack 更新

--- Amwork/src/main/java/com/hades/mylibrary/mvp/interf/IModelCallBack.java
+++ Amwork/src/main/java/com/hades/mylibrary/mvp/interf/IModelCallBack.java
@@ -4,7 +4,7 @@ package com.hades.mylibrary.mvp.interf;
  * Created by Hades on 2017/6/29.
  */
 
-public interface IModelCallBack {
+public interface IModelCallBack extends IRootModel {
     void onSuccess(String data);
     void onFailed(String message);
 }
